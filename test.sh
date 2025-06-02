adc6abb22b6aa7350cf3fe08f6773346eee8bcfb
Ben Poulson <ben.poulson@nexelity.com>
2025-06-02 19:01:20 +0100

    Docker file changes

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+composer validate --no-check-all --no-check-publish
 php -d memory_limit=-1 vendor/bin/phpstan analyse -c phpstan.neon --memory-limit=2G
 php -d memory_limit=-1 vendor/bin/phpunit
